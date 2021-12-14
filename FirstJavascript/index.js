51f7102d2629b2d92ecc07e7fa9d9235d55ce05d
Rene-Zaltbommel <91453184+Rene-Zaltbommel@users.noreply.github.com>
2021-12-14 11:12:25 +0100

    Add files via upload

---
+++ FirstJavascript/index.js
@@ -0,0 +1,37 @@
+console.log("Hello Winc Academy");
+
+let rene='Rene';
+
+console.log(rene);
+
+let numb= '4+4';
+
+let stri= '4+4';
+
+let y = 8;
+let x = y + 2;
+
+console.log(x);
+
+let z = y-2;
+
+console.log(z);
+
+let a = y/3;
+
+let b = y*6;
+
+let c = y%50;
+
+
+console.log(a,b,c)
+
+console.log(typeof 36);
+
+console.log(typeof 'thirtysix');
+
+
+
+
+
+
